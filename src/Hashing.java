c18fc30e6326b819628e49bbd5d89ff3f1567072
Rajkin-Hossain <rajkin.hossain@gmail.com>
2019-05-19 23:03:55 +0600

    Add computePrimes and getHash functions

--- src/Hashing.java
+++ src/Hashing.java
@@ -1,12 +1,22 @@
-
 public class Hashing {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    long prime = 33;
+    long [] primes;
+    
+    public void computePrimes(){
+        primes[1] = prime;
+        for(int i = 2; i<primes.length; i++){
+            //primes[i] = (primes[i-1] * prime);
+            primes[i] = (primes[i-1] << (long) 5) + primes[i-1];
+        }
+    }
+    
+    public long getHash(String name){
+        long hash = 0;
+        for(int i = 0; i<name.length(); i++){
+            //hash = (hash * prime) + name.charAt(i);
+            hash = ((hash << (long)5) + hash) + name.charAt(i);
+        }
+        
+        return hash;
+    }
 }
