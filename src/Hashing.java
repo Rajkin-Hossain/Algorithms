90c891e4903cdf328b6c54a1fccfc781ac9648cb
Rajkin-Hossain <rajkin.hossain@gmail.com>
2019-05-19 23:19:34 +0600

    Add isFound function

--- src/Hashing.java
+++ src/Hashing.java
@@ -19,4 +19,40 @@ public class Hashing {
         
         return hash;
     }
+    
+    public boolean isFound(String text, String pattern) {
+    	int textLength = text.length();
+    	int patternLength = pattern.length();
+
+        primes = new long[textLength + 1];
+        computePrimes();
+        
+        long requiredHash = getHash(pattern);
+        long [] prefixHashes = new long[textLength];
+        
+        long hash = 0;
+        for(int i = 0; i<Math.min(textLength, patternLength); i++){
+            //hash = (hash * prime) + A.charAt(i);
+            hash = ((hash << (long)5) + hash)  + text.charAt(i);
+            prefixHashes[i] = hash;
+        }
+        
+        if(requiredHash == hash) return true;
+        
+        int start = 0;
+        for(int i = Math.min(textLength, patternLength); i<textLength; i++){
+            //hash = (hash * prime) + A.charAt(i);
+            hash = ((hash << (long)5) + hash)  + text.charAt(i);
+            
+            prefixHashes[i] = hash;
+            
+            if((hash - (prefixHashes[start] * primes[patternLength])) == requiredHash) {
+            	return true;
+            }
+            
+            start++;
+        }
+        
+        return false;
+    }
 }
