4998c7ff10a160a1ecf397a7ca0b7d0a02b8c4c3
Rajkin-Hossain <rajkin.hossain@gmail.com>
2019-08-16 02:48:53 +0600

    Add SortWithComparator

--- src/CodeSnippet.java
+++ src/CodeSnippet.java
@@ -17,4 +17,34 @@ public class CodeSnippet {
 	PriorityQueue<Integer> getReverseOrderPriorityQueue() {
 		return new PriorityQueue<>(Collections.reverseOrder());
 	}
+	
+	void SortWithComparator(ArrayList<Pair> intervals) {
+        Collections.sort(intervals, new Comparator<Pair>() {
+            
+        	// Current is actually this
+        	// if you need to return current just direct use -1
+        	// if you need to return next just direct use 1
+        	
+        	/* if Pair class has Wrapper class 
+        	 	If you need to return current you can use like current.compareTo(next)
+        	 	BE CAREFULL: Wrapper class equality check like == wont work so, 
+        	 	use instead equals method for Wrapper class
+        	*/
+        	
+            public int compare(Pair current, Pair next) {
+            	
+            	// sort based in ascending order
+                return current.start - next.start;
+            }
+        });
+	}
+	
+	class Pair{
+		int start, end;
+
+		public Pair(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+	}
 }
